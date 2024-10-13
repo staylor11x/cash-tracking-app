7c7b813393bee210139859bc5fd77d60bbaeae98
Scott Taylor <scott2013taylor@gmail.com>
2024-10-13 22:21:38 +0100

    files re-added

---
+++ CashTrackingApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CashTrackingApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CashTrackingApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CashTrackingApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
